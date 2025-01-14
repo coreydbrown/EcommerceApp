1dc0e952ee8a28c8ced3bb466e950569532d95c4
Corey Brown <corey.brown.ny@gmail.com>
2025-01-13 20:10:47 -0500

    Created Products controller and method to get all products.

---
+++ EcommerceApp.Api/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using EcommerceApp.Core.Entities;
+using EcommerceApp.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
+
+namespace EcommerceApp.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext context;
+
+        public ProductsController(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProduct()
+        {
+            return await context.Products.ToListAsync();
+        }
+    }
+}
